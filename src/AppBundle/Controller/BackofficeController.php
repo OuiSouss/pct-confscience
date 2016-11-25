ab1cf637d8ec8602a4da41ecbf272e1717260950
Robin Hardy <rhardy@emi.u-bordeaux1.fr>
2016-11-25 15:57:23 +0100

    Ajout de la création d'évènement, sans sauvegarde dans la base de données qui n'est pas encore créée

--- src/AppBundle/Controller/BackofficeController.php
+++ src/AppBundle/Controller/BackofficeController.php
@@ -4,6 +4,14 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use AppBundle\Entity\Event;
+
 
 class BackofficeController extends Controller{
 	
@@ -13,4 +21,34 @@ class BackofficeController extends Controller{
 	public function indexAction(){
 		return $this->render('backoffice.html.twig');
 	}
+	
+	/**
+	* @Route("/back-office/creation-evenement", name="creation-evenement")
+	*/
+	public function newAction(Request $request){
+		$event = new Event();
+		
+		$form = $this->createFormBuilder($event)
+			->add('titre', TextType::class)
+			->add('date', DateType::class)
+			->add('image', TextType::class, array('required' => false))
+			->add('uploadImage', SubmitType::class, array('label' => 'Charger l\'image'))
+			->add('description', TextType::class)
+			->add('save', SubmitType::class, array('label' => 'Créer évènement'))
+			->add('draft', SubmitType::class, array('label' => 'Sauvegarder brouillon'))
+			->getForm();
+		
+		$form->handleRequest($request);
+		
+		if($form->isSubmitted()){
+			$event = $form->getData();
+			
+			//suvegarder dans la base de données
+			
+			return $this->redirectToRoute('homepage');
+		}
+		
+		return $this->render('backoffice/new.html.twig', array('form' => $form->createView(),));
+	}
+	
 }
---
+++ src/AppBundle/Entity/Event.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Event{
+	
+	/**
+	* @Assert\NotBlank()
+	*/
+	protected $titre;
+	
+	protected $date;
+	
+	protected $image;
+	
+	protected $description;
+	
+	
+	public function getTitre(){
+		return $this->titre;
+	}
+	
+	public function setTitre($titre){
+		$this->titre = $titre;
+	}
+	
+	public function getDate(){
+		return $this->date;
+	}
+	
+	public function setDate(\DateTime $date = null){
+		$this->date = $date;
+	}
+	
+	public function getImage(){
+		return $this->image;
+	}
+	
+	public function setImage($image){
+		$this->image = $image;
+	}
+	
+	public function getDescription(){
+		return $this->description;
+	}
+	
+	public function setDescription($description){
+		$this->description = $description;
+	}
+	
+}
