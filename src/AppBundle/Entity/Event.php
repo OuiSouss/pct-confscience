a3b89ae05ea2ac2a81feb0d900b6e3dc84446c6a
Your Name <a@a>
2016-12-05 15:11:09 +0100

    ajout des configurations d'administration avec fosuserbundle

--- src/AppBundle/Entity/Event.php
+++ src/AppBundle/Entity/Event.php
@@ -2,64 +2,189 @@
 
 namespace AppBundle\Entity;
 
-use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
 
-class Event{
+/**
+ * Event
+ *
+ * @ORM\Table(name="event")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EventRepository")
+ */
+class Event
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
-	/**
-	* @Assert\NotBlank()
-	*/
-	protected $titre;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, unique=true)
+     */
+    private $title;
 
-	protected $date;
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetimetz")
+     */
+    private $date;
 
-	protected $image;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255)
+     */
+    private $image;
 
-	protected $description;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, unique=true)
+     */
+    private $description;
 
-	protected $published;
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="published", type="boolean")
+     */
+    private $published;
 
-	public function getTitre(){
-		return $this->titre;
-	}
 
-	public function setTitre($titre){
-		$this->titre = $titre;
-	}
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	public function getDate(){
-		return $this->date;
-	}
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Event
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
 
-	public function setDate(\DateTime $date = null){
-		$this->date = $date;
-	}
+        return $this;
+    }
 
-	public function getImage(){
-		return $this->image;
-	}
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
-	public function setImage($image){
-		$this->image = $image;
-	}
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Event
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
 
-	public function getDescription(){
-		return $this->description;
-	}
+        return $this;
+    }
 
-	public function setDescription($description){
-		$this->description = $description;
-	}
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
 
-	public function Pusblished(){
-		$this->published = true;
-	}
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Event
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
 
-	public function unPublished(){
-		$this->published = false;
-	}
+        return $this;
+    }
 
-	public function isPublished(){
-		return $this->published;
-	}
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Event
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set published
+     *
+     * @param boolean $published
+     *
+     * @return Event
+     */
+    public function setPublished($published)
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    /**
+     * Get published
+     *
+     * @return bool
+     */
+    public function getPublished()
+    {
+        return $this->published;
+    }
 }
+
