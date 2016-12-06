a2147de5290ed625abb30c1455bed54c341f3b91
Paul Jose Vedrenne <pjose@etu.u-bordeaux.fr>
2016-12-06 17:11:26 +0100

    MAJ mode draft

--- src/AppBundle/Controller/DraftController.php
+++ src/AppBundle/Controller/DraftController.php
@@ -4,86 +4,146 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+use AppBundle\Entity\Event;
 
 class DraftController extends Controller
 {
   /**
-   * @Route("/draft/{u}", name="draft_home_page")
+   * @Route("/draft", name="draft_index")
    */
     public function indexAction()
     {
-        $repository = $this->getDoctrine()->getManager()->getRepository('AppBundle:Draft');
-        $user = $this->get('security.context')->getToken()->getUser();//Récupération de l'utilisateur courant
+        $repository = $this->getDoctrine()->getManager()->getRepository('AppBundle:Event');
+        $user = $this->get('security.token_storage')->getToken()->getUser();//Récupération de l'utilisateur courant
         /*Listing des evenements en mode draft*/
-        $listDraftEvents = $repository->findBy(array('user'=>$user));
-        foreach ($listDraftEvent as $draft){
-          echo $draft->getContent();//Récupération des evenements marqué comme draft
-        }
+        $listDraftEvents = $repository->loadAllDraftUser($user);
 
-        return $this->render('draft/index.html.twig');// Créer la vue d'indexation des drafts
+        if($listDraftEvents == null)
+          return $this->render('draft/index.html.twig',array('empty'=>true));// Créer la vue d'indexation des drafts
+        return $this->render('draft/index.html.twig',array('all_draft'=>$listDraftEvents));
     }
 
     /**
-    * @Route("/publish-draft", name="draft_publish")
+    * @Route("/publish-draft/{id}", name="draft_publish")
     */
-    public function publishDraft(Request $request){
-      $id_draft = $request->attributes->get('id_draft');
-      $draft = getDraft($id_draft);
+    public function publishDraft($id){
 
+      $repository_event = $this->getDoctrine()->getRepository('AppBundle:Event');
+      $draft = $repository_event->find($id);
       if($draft == null){
-        $response = new Response();
-        $response->setContent("Error 404");// Associer une vue à une erreur 404
-        $response->setStatusCode(Response::HTTP_NOT_FOUND);
-        return $response;
+        throw $this->createNotFoundException('L\'evenement demandé n\'existe pas');
       }
 
-      $event = $draft->getEvent();
+      if(!$draft->getPublished()){
+        $draft->setPublished(true);
+      }
 
-      $event->Published();// Modifier en fonction de l'entité Event
-
-      $em->remove($draft);
-      $em->flush();
-
-      return $this->redirectToRoute('draft_home_page');
-    }
-
-
-    //A implementer dans la EventController
-  /*  public function addDraftAction(User $u, Event $event){
-      $draft = new Draft();
-      $draft->setUser($u);
-      $draft->setEvent($event);
-
-      $em = $this->getDoctine()->getManager();
-
+      $em = $this->getDoctrine()->getManager();
       $em->persist($draft);
       $em->flush();
-    }*/
+
+      return $this->redirectToRoute('draft_index');
+    }
 
     /**
-    * @Route("/remove-draft", name="draft_remover")
+    * @Route("/modify-draft/{id}", name="draft_modify")
     */
-    public function removeDraftAction(Request $request){
-      $draft = $this->getDraft($request->attributes->get($id_draft));
+    public function modifyDraftAction($id,Request $request){
+      $draft = $this->getDoctrine()->getRepository('AppBundle:Event')->find($id);
+      $event = new Event();
+      if($draft != null)
+      {
+        if(!$draft->getPublished()){
+          $form = $this->createFormBuilder($event)
+            ->add('titre', TextType::class, array('data'=>$draft->getTitre()))
+            ->add('date', DateType::class, array('data'=>$draft->getDate()))
+            ->add('image', FileType::class, array('required' => false, 'label' => 'Charger une image'))
+            ->add('description', TextareaType::class, array('label' => 'Description', 'data' => $draft->getDescription()))
+            ->add('published', CheckboxType::class, array('required' => false, 'label' => 'Publier'))
+            ->add('save', SubmitType::class, array('label' => 'Modifier'))
+            ->getForm();
 
-      if($draft != null){
+          $form->handleRequest($request);
+
+          if($form->isSubmitted() && $form->isValid()){
+      			$event = $form->getData();
+
+      			$event->setUser($this->get('security.token_storage')->getToken()->getUser());
+      			// Base de données en attente...
+      			 /** @var Symfony\Component\HttpFoundation\File\UploadedFile $image */
+      			$image = $event->getImage();
+
+            if($image != null){
+      			     $fileName = md5(uniqid()).'.'.$image->guessExtension();
+
+      			        $image->move(
+                      $this->getParameter('upload_images_directory'),
+                      $fileName
+                  );
+
+      			    $event->setImage($fileName);
+            }
+
+            else{
+              $event->setImage($draft->getImage());
+            }
+      			$em = $this->getDoctrine()->getManager();
+            $em->remove($draft);
+      			$em->persist($event);
+      			$em->flush();
+
+      			return $this->redirectToRoute('draft_index');
+          }
+        }
+      }
+  		return $this->render('draft/modify.html.twig', array('form' => $form->createView(),));
+    }
+
+    /**
+    * @Route("/remove-draft/{id}", name="remove_draft")
+    */
+    public function removeDraftAction($id){
+      $draft = $this->getDoctrine()->getRepository('AppBundle:Event')->find($id);
+
+      if($draft == null){
+        throw $this->createNotFoundException('L\'evenement demandé n\'existe pas');
+      }
+
+      if(!$draft->getPublished()){
         $em = $this->getDoctrine()->getManager();
         $em->remove($draft);
         $em->flush();
       }
-
-      return $this->redirectToRoute('draft_home_page');//Redirection vers la page d'acceuil du mode draft (Provoque une maj de l'indexation)
+      return $this->redirectToRoute('draft_index');//Redirection vers la page d'acceuil du mode draft (Provoque une maj de l'indexation)
 
 
     }
 
-    private function getDraft($id){
-      $em = $this->getDoctrine()->getManager();
-      $repository = $em->getRepository('AppBundle:Draft');
+    /**
+    * @Route("/unpublished/{id}", name="unpublised_event")
+    */
+    public function unpublishedAction($id){
+      $draft = $this->getDoctrine()->getRepository('AppBundle:Event')->find($id);
 
-      $draft = $repository->find($id);
+      if($draft == null){
+        throw $this->createNotFoundException('L\'evenement demandé n\'existe pas');
+      }
 
-      return $draft;
+      if($draft->getPublished()){
+        $em = $this->getDoctrine()->getManager();
+        $draft->setPublished(false);
+        $em->persist($draft);
+        $em->flush();
+      }
+      return $this->redirectToRoute('draft_index');//Redirection vers la page d'acceuil du mode draft (Provoque une maj de l'indexation)
+
     }
 
 }
---
+++ src/AppBundle/Repository/EventRepository.php
@@ -0,0 +1,45 @@
+<?php
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class EventRepository extends EntityRepository
+{
+    public function loadAllPublishedEvent()
+    {
+        return $this->createQueryBuilder('e')
+            ->where('e.published = 1')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function loadEventById($id){
+      return $this->createQueryBuilder('e')
+            ->where('e.id = :id')
+            ->setParameter('id',$id)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function loadAllDraftUser($user)
+    {
+      return $this->createQueryBuilder('e')
+        ->where('e.published = 0 AND e.user = :user')
+        ->setParameter('user',$user)
+        ->getQuery()
+        ->getResult();
+    }
+
+    /*
+    public function loadPublishedEventByUsername($user)
+    {
+      return $this->createQueryBuilder('e')
+          ->where('e.user = :user AND e.published = 1')
+          ->setParameter('user',$user)
+          ->getQuery()
+          ->getResult();
+
+    }*/
+}
+
+ ?>
