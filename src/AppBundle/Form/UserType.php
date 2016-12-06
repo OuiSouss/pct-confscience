7316729a33d875200e6561bbddaa44940e7c0d7a
Paul Jose Vedrenne <pjose@etu.u-bordeaux.fr>
2016-12-06 19:22:50 +0100

    MAJ general des vues + MAJ css

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -15,11 +15,11 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class)
-            ->add('username', TextType::class)
+            ->add('username', TextType::class, array('label'=>"Nom d'utilisateur"))
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password'),
+                'first_options'  => array('label' => 'Mot de passe'),
+                'second_options' => array('label' => 'Confirmez mot de passe'),
             )
         );
     }
