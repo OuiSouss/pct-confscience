93769ef9440d114c08d6fb1968d8c384998cd935
Your Name <a@a>
2016-12-14 11:33:53 +0100

    esperons que cette fois ci il y a tout pour utiliser le fos user bundle

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,15 +4,15 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitbdff5614a5040c9bc96440c34031e35c
+class ComposerStaticInit86f79e31517b76aafa722981eb9bc317
 {
     public static $files = array (
         '92c8763cd6170fce6fcfe7e26b4e8c10' => __DIR__ . '/..' . '/symfony/phpunit-bridge/bootstrap.php',
         '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '023d27dca8066ef29e6739335ea73bad' => __DIR__ . '/..' . '/symfony/polyfill-php70/bootstrap.php',
         'bd9634f2d41831496de0d3dfe4c94881' => __DIR__ . '/..' . '/symfony/polyfill-php56/bootstrap.php',
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '6a47392539ca2329373e0d33e1dba053' => __DIR__ . '/..' . '/symfony/polyfill-intl-icu/bootstrap.php',
         '32dcc8afd4335739640db7d200c1971d' => __DIR__ . '/..' . '/symfony/polyfill-apcu/bootstrap.php',
     );
@@ -55,10 +55,15 @@ class ComposerStaticInitbdff5614a5040c9bc96440c34031e35c
         array (
             'Incenteev\\ParameterHandler\\' => 27,
         ),
+        'F' => 
+        array (
+            'FOS\\UserBundle\\' => 15,
+        ),
         'D' => 
         array (
             'Doctrine\\Instantiator\\' => 22,
             'Doctrine\\Common\\Cache\\' => 22,
+            'Doctrine\\Common\\Annotations\\' => 28,
             'Doctrine\\Common\\' => 16,
             'Doctrine\\Bundle\\DoctrineCacheBundle\\' => 36,
             'Doctrine\\Bundle\\DoctrineBundle\\' => 31,
@@ -154,6 +159,10 @@ class ComposerStaticInitbdff5614a5040c9bc96440c34031e35c
         array (
             0 => __DIR__ . '/..' . '/incenteev/composer-parameter-handler',
         ),
+        'FOS\\UserBundle\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/friendsofsymfony/user-bundle',
+        ),
         'Doctrine\\Instantiator\\' => 
         array (
             0 => __DIR__ . '/..' . '/doctrine/instantiator/src/Doctrine/Instantiator',
@@ -161,6 +170,10 @@ class ComposerStaticInitbdff5614a5040c9bc96440c34031e35c
         'Doctrine\\Common\\Cache\\' => 
         array (
             0 => __DIR__ . '/..' . '/doctrine/cache/lib/Doctrine/Common/Cache',
+        ),
+        'Doctrine\\Common\\Annotations\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/annotations/lib/Doctrine/Common/Annotations',
         ),
         'Doctrine\\Common\\' => 
         array (
@@ -217,10 +230,6 @@ class ComposerStaticInitbdff5614a5040c9bc96440c34031e35c
             array (
                 0 => __DIR__ . '/..' . '/doctrine/collections/lib',
             ),
-            'Doctrine\\Common\\Annotations\\' => 
-            array (
-                0 => __DIR__ . '/..' . '/doctrine/annotations/lib',
-            ),
         ),
     );
 
@@ -243,11 +252,11 @@ class ComposerStaticInitbdff5614a5040c9bc96440c34031e35c
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitbdff5614a5040c9bc96440c34031e35c::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitbdff5614a5040c9bc96440c34031e35c::$prefixDirsPsr4;
-            $loader->fallbackDirsPsr4 = ComposerStaticInitbdff5614a5040c9bc96440c34031e35c::$fallbackDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInitbdff5614a5040c9bc96440c34031e35c::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInitbdff5614a5040c9bc96440c34031e35c::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit86f79e31517b76aafa722981eb9bc317::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit86f79e31517b76aafa722981eb9bc317::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr4 = ComposerStaticInit86f79e31517b76aafa722981eb9bc317::$fallbackDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit86f79e31517b76aafa722981eb9bc317::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit86f79e31517b76aafa722981eb9bc317::$classMap;
 
         }, null, ClassLoader::class);
     }
